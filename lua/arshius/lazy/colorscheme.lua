a2aaa8973d7f8bd418fec80cbf43b2b7b0ae6ef8
Danuys <danuys@nusaciptaalumindo.com>
2025-08-26 11:58:38 +0700

    adding the rest of primeagen lua files

--- lua/arshius/lazy/colorscheme.lua
+++ lua/arshius/lazy/colorscheme.lua
@@ -1,4 +1,4 @@
-function ColorSchemes(color)
+function ColorMyVim(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
@@ -10,11 +10,71 @@ function ColorSchemes(color)
 	--vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
 	--vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
 	--vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
-
-	print("changed color scheme")
 end
 
 return {	
+    {
+        "erikbackman/brightburn.vim",
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        opts = {},
+        config = function ()
+            ColorMyVim()
+        end,
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        name = "gruvbox",
+        config = function ()
+            require("gruvbox").setup({
+                terminal_colors = true, -- add neovim terminal colors
+                undercurl = true,
+                underline = false,
+                bold = true,
+                italic = {
+                    strings = false,
+                    emphasis = false,
+                    comments = false,
+                    operators = false,
+                    folds = false,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_intend_guides = false,
+                inverse = true, -- invert background for search, diffs, statuslines and errors
+                contrast = "", -- can be "hard", "soft", or empty string
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = false,
+            })
+        end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        config = function ()
+            require("tokyonight").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                transparent = true, -- Enable this to disable setting the background color
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                styles = {
+                    -- Style to be applied to different syntax groups
+                    -- Value is any valid attr-list value for `:help nvim_set_hl`
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    -- Background styles. Can be "dark", "transparent" or "normal"
+                    sidebars = "dark", -- style for sidebars, see below
+                    floats = "dark", -- style for floating windows
+                },
+            })
+        end,
+    },
 	{
 		"rose-pine/neovim",
 		name = "rose-pine",
@@ -29,9 +89,12 @@ return {
 					--TelescopeResultsNormal = { fg = "subtle", bg = "none" },
 				},
 				disable_background = true,
+                styles = {
+                    italic = false,
+                },
 			})
 			--vim.cmd('colorscheme rose-pine')
-			ColorSchemes()
+			ColorMyVim()
 		end
 	},
 }
--- lua/arshius/lazy/harpoon.lua
+++ lua/arshius/lazy/harpoon.lua
@@ -1,47 +1,68 @@
 return {
-	{
-		"theprimeagen/harpoon",
-		branch = "harpoon2",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local harpoon = require("harpoon")
-			harpoon:setup()
+    {
+        "theprimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require("harpoon")
+            harpoon:setup()
 
-			vim.keymap.set("n", "<leader>A", function()
-				harpoon:list():prepend()
-			end)
-			vim.keymap.set("n", "<leader>a", function()
-				harpoon:list():add()
-			end)
-			vim.keymap.set("n", "<C-e>", function()
-				harpoon.ui:toggle_quick_menu(harpoon:list())
-			end)
+            --local conf = require("telescope.config").values
+            --local function toggle_telescope(harpoon_files)
+            --    local file_paths = {}
+            --    for _, item in ipairs(harpoon_files.items) do
+            --        table.insert(file_paths, item.value)
+            --    end
 
-			vim.keymap.set("n", "<C-Insert>", function()
-				harpoon:list():select(1)
-			end)
-			vim.keymap.set("n", "<C-End>", function()
-				harpoon:list():select(2)
-			end)
-			vim.keymap.set("n", "<C-Down>", function()
-				harpoon:list():select(3)
-			end)
-			vim.keymap.set("n", "<C-PageDown>", function()
-				harpoon:list():select(4)
-			end)
+            --    require("telescope.pickers").new({}, {
+            --        prompt_title = "Harpoon",
+            --        finder = require("telescope.finders").new_table({
+            --            results = file_paths,
+            --        }),
+            --        previewer = conf.file_previewer({}),
+            --        sorter = conf.generic_sorter({}),
+            --    }):find()
+            --end
 
-			vim.keymap.set("n", "<leader><C-Insert>", function()
-				harpoon:list():replace_at(1)
-			end)
-			vim.keymap.set("n", "<leader><C-End>", function()
-				harpoon:list():replace_at(2)
-			end)
-			vim.keymap.set("n", "<leader><C-Down>", function()
-				harpoon:list():replace_at(3)
-			end)
-			vim.keymap.set("n", "<leader><C-PageDown>", function()
-				harpoon:list():replace_at(4)
-			end)
-		end,
-	}
+            --vim.keymap.set("n", "<C-e>", function() 
+            --    toggle_telescope(harpoon:list()) 
+            --end, { desc = "Open harpoon window" })
+
+            vim.keymap.set("n", "<C-e>", function()
+                harpoon.ui:toggle_quick_menu(harpoon:list())
+            end)
+            vim.keymap.set("n", "<leader>A", function()
+                harpoon:list():prepend()
+            end)
+            vim.keymap.set("n", "<leader>a", function()
+                harpoon:list():add()
+            end)
+
+            vim.keymap.set("n", "<C-Insert>", function()
+                harpoon:list():select(1)
+            end)
+            vim.keymap.set("n", "<C-End>", function()
+                harpoon:list():select(2)
+            end)
+            vim.keymap.set("n", "<C-Down>", function()
+                harpoon:list():select(3)
+            end)
+            vim.keymap.set("n", "<C-PageDown>", function()
+                harpoon:list():select(4)
+            end)
+
+            vim.keymap.set("n", "<leader><C-Insert>", function()
+                harpoon:list():replace_at(1)
+            end)
+            vim.keymap.set("n", "<leader><C-End>", function()
+                harpoon:list():replace_at(2)
+            end)
+            vim.keymap.set("n", "<leader><C-Down>", function()
+                harpoon:list():replace_at(3)
+            end)
+            vim.keymap.set("n", "<leader><C-PageDown>", function()
+                harpoon:list():replace_at(4)
+            end)
+        end,
+    }
 }
